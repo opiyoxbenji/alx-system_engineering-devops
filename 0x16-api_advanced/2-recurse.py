9b4c54796126f0963e01ddbe52b6bb5f5a7e0ad8
opiyos <opiyosbenji@gmail.com>
2024-02-13 19:49:58 +0300

    an update

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
 """
-Returns a list containing the titles of all hot articles for a given subreddit
-If no results are found for the given subreddit, the function should return None
+Function that queries the Reddit API and returns a list containing the titles of all hot articles
 """
-import json
+
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None, count=0):
+def recurse(subreddit, hot_list=[], after=None):
     """
-    Queries the Reddit API
+    Retrieve titles of all hot articles for a given subreddit
     """
-    if after is None:
-        mini_URL = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    else:
-        mini_URL = "https://www.reddit.com/r/{}/hot.json?after={}".format(
-                subreddit, after)
-    subreddit_info = requests.get(mini_URL,
-                                  headers={"user-agent": "user"},
-                                  allow_redirects=False).json()
-    if "data" not in subreddit_info and hot_list == []:
-        return None
-    children = subreddit_info.get("data").get("children")
-    for child in children:
-        hot_list.append(child.get("data").get("children"))
-        count += 1
-    after = subreddit_info.get("data").get("after")
-    if after is None:
-        return hot_list
-    return (recurse(subreddit, hot_list, after, count))
+    headers = {"User-Agent": "MyRedditClient/1.0"}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+    if after:
+        url += f"?after={after}"
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        return (None)
+    data = response.json()
+    posts = data.get("data", {}).get("children", [])
+    titles = [post["data"]["title"] for post in posts]
+    hot_list.extend(titles)
+    next_page = data["data"].get("after")
+    if next_page:
+        return (get_hot_articles(subreddit, hot_list, next_page))
+    return (hot_list)
