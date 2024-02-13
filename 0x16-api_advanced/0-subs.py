eaa0bd96933408a20affb77e534e2ff263295702
opiyos <opiyosbenji@gmail.com>
2024-02-13 20:21:09 +0300

    an update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,14 +10,12 @@ def number_of_subscribers(subreddit):
     Returns the number of subs or 0 if the sbureddit is invalid
     """
     url = "https://reddit.com/r/{}/about.json".format(subreddit)
-    user_agent = "2024"
     headers = {
-            "User-Agent": user_agent
+            "User-Agent": "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion"
     }
 
     response = requests.get(url, allow_redirects=False, headers=headers)
-    if response.status_code == 200:
-        data = response.json().get('data')
-        return (data.get('subscribers'))
-    else:
-        return (0)
+    if response.status_code == 404:
+        return 0
+    data = response.json().get('data')
+    return data.get('subscribers')
