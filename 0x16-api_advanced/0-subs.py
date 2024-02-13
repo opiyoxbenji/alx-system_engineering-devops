bd772f8111141accb29ef1f1f5e3e0f6d2791a40
opiyoxbenji <opiyosbenji@gmail.com>
2024-02-13 09:10:09 +0000

    changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,15 +9,15 @@ def number_of_subscribers(subreddit):
     """
     Returns the number of subs or 0 if the sbureddit is invalid
     """
-    url = f"https://reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "OpenSourceSubredditSubscriberInfo/2024"}
-    response = requests.get(url, allow_redirections=False, headers=headers)
+    url = "https://reddit.com/r/{}/about.json".format(subreddit)
+    user_agent = "OpenSourceSubredditSubscriberInfo/2024"
+    headers = {
+            "User-Agent": user_agent
+    }
 
+    response = requests.get(url, allow_redirections=False, headers=headers)
     if response.status_code == 200:
-        try:
-            data = response.json()
-            return (data["data"]["subscribers"])
-        except (KeyError, TypeError):
-            return (0)
+        data = response.json().get('data')
+        return (data.get('subscribers'))
     else:
         return (0)
